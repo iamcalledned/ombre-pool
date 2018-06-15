fc6be2a483ccddba77b48e1df777cb4f2142050d
Ned <iamcalledned@gmail.com>
2018-06-15 10:18:46 -0400

    Update config.js

--- website/config.js
+++ website/config.js
@@ -1,17 +1,17 @@
-var api = "http://18.222.115.198:8117";
+var api = "http://18.191.192.173:8117";
 
-var poolHost = "18.222.115.198";
+var poolHost = "18.191.192.173";
 
-var irc = "irc.freenode.net/#sumokoin";
+var irc = "irc.freenode.net/#ombre";
 
-var email = "support@sumokoin.ch";
+var email = "support@ombre.io";
 
 var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://explorer.sumokoin.com/block/{id}";
+var blockchainExplorer = "https://explorer.ombre.io/block/{id}";
 
-var transactionExplorer = "http://explorer.sumokoin.com/tx/{id}";
+var transactionExplorer = "https://explorer.ombre.io/tx/{id}";
 
 var themeCss = "themes/default-theme.css";
